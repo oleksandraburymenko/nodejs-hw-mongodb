0c45ea6debd0d09d781e3f00072e049376b409eb
oleksandraburymenko <olexandralife@gmail.com>
2024-07-27 17:34:29 +0300

    add routers and controllers

--- src/utils/googleOAuth2.js
+++ src/utils/googleOAuth2.js
@@ -1,21 +1,43 @@
 import { OAuth2Client } from "google-auth-library"
 import {readFile} from "node:fs/promises";
 import path from "node:path";
+import createHttpError from "http-errors";
 
-import { env } from "node:process";
+import { ENV_VARS, env } from "./env.js";
 
 const googleOAuthSettingPath = path.resolve("google-oauth.json");
 const googleOAuthSettings = JSON.parse(await readFile(googleOAuthSettingPath));
 
-const clientId = env("GOOGLE_AUTH_CLIENT_ID");
-const clientSecret = env("GOOGLE_AUTH_CLIENT_SECRET");
+const clientId = env(ENV_VARS.GOOGLE_AUTH_CLIENT_ID);
+const clientSecret = env(ENV_VARS.GOOGLE_AUTH_CLIENT_SECRET);
 
 const googleOAuthClient = new OAuth2Client({
     clientId,
     clientSecret,
-    redirectUri: googleOAuthSettings.web.redirect_uris[0]
-})
+    redirectUri: googleOAuthSettings.web.redirect_uris[0],
+});
 
+export const validateGoogleOAuthCode = async (code) => {
+    const response = await googleOAuthClient.getToken(code);
+    if(!response.tokens.id_token) {
+        throw createHttpError(401, "Google OAuth code invalid");
+
+    }
+    const ticket = await googleOAuthClient.verifyIdToken({
+        idToken: response.tokens.id_token
+    });
+
+    return ticket;
+
+}
+
+export const getGoogleOAuthName = ({given_name, family_name}) => {
+    if(!given_name) return "User";
+    const name = family_name ? `${given_name} ${family_name}` : given_name;
+
+    return name;
+
+}
 
 export const generateAuthUrl = ()=> {
     return googleOAuthClient.generateAuthUrl({
