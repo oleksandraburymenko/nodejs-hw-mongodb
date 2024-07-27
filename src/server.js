0cf29ac5726bd0ff662a52c2e0eff711bf430154
oleksandraburymenko <olexandralife@gmail.com>
2024-07-27 23:05:58 +0300

    add documentation

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ import { env } from './utils/env.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 
 dotenv.config();
@@ -22,6 +23,7 @@ export const setupServer = () => {
   app.use(cookieParser());
   app.use(express.json());
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
   app.use(
     pino({
       transport: {
